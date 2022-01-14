30643d5be9244e7f5f434048286c98160ddf7642
Recep Bozdemir <48210122+recepbozdemr@users.noreply.github.com>
2022-01-14 21:29:25 +0300

    Version1

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concreate;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfArticleRepository : EfEntityRepositoryBase<Article>,IArticleRepository
+
+    {
+        public EfArticleRepository(DbContext context): base(context)
+        {
+
+
+
+        }
+
+
+    }
+}
---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+   public interface IResult
+    {
+
+        public ResultStatus ResultStatus { get; } //Success veya Error olarak kullanacağız
+        public string Message { get;  }
+        public Exception Exception { get;  }
+
+    }
+}
